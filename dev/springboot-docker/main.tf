06f6ce65800d59f3177c82f53a3c47f6cb5151f3
Ming Fang <mingfang@mac.com>
2020-06-26 15:02:08 -0400

    add ingress

--- dev/springboot-docker/main.tf
+++ dev/springboot-docker/main.tf
@@ -4,7 +4,8 @@ resource "k8s_core_v1_namespace" "this" {
   }
 }
 
-locals {
+module "springboot" {
+  source = "git::https://github.com/mingfang/terraform-k8s-modules.git//archetypes/deployment-service"
   parameters = {
     name                 = var.name
     namespace            = k8s_core_v1_namespace.this.metadata[0].name
@@ -15,7 +16,7 @@ locals {
 
     containers = [
       {
-        name      = "registry"
+        name      = "springboot"
         image     = var.image
         env       = var.env
         resources = var.resources
@@ -24,7 +25,27 @@ locals {
   }
 }
 
-module "deployment-service" {
-  source     = "git::https://github.com/mingfang/terraform-k8s-modules.git//archetypes/deployment-service"
-  parameters = merge(local.parameters, var.overrides)
+resource "k8s_networking_k8s_io_v1beta1_ingress" "this" {
+  metadata {
+    annotations = {
+      "kubernetes.io/ingress.class"              = "nginx"
+      "nginx.ingress.kubernetes.io/server-alias" = "springboot.*"
+    }
+    name      = module.springboot.name
+    namespace = k8s_core_v1_namespace.this.metadata[0].name
+  }
+  spec {
+    rules {
+      host = "${module.springboot.name}-${var.namespace}"
+      http {
+        paths {
+          backend {
+            service_name = module.springboot.name
+            service_port = module.springboot.service.spec[0].ports[0].port
+          }
+          path = "/"
+        }
+      }
+    }
+  }
 }
